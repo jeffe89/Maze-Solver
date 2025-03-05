c627b49078468474b521ef622a140cdef54c92e1
jeffe89 <jeffe8989@gmail.com>
2025-03-04 21:37:08 -0800

    Minor updates

--- cell.py
+++ cell.py
@@ -64,15 +64,13 @@ class Cell:
     #Method to draw a path between two cells
     def draw_move(self, to_cell, undo=False):
 
-        #Calculate the center values for first cell
-        half_length = abs(self._x2 - self._x1) // 2
-        x_center = half_length + self._x1
-        y_center = half_length + self._y1
+        # Calculate the center values for first cell
+        x_center = (self._x1 + self._x2) // 2
+        y_center = (self._y1 + self._y2) // 2
 
-        #Calculate the center values for second cell
-        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
-        x_center2 = half_length2 + to_cell._x1
-        y_center2 = half_length2 + to_cell._y1
+        # Calculate the center values for second cell
+        x_center2 = (to_cell._x1 + to_cell._x2) // 2
+        y_center2 = (to_cell._y1 + to_cell._y2) // 2
 
         #Set fill color according to undo flag
         fill_color = "red"
